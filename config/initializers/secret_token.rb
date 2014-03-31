7a481db8cb18419b31e478943d621f578848ae75
kevinferri <kevinferri@gmail.com>
2014-03-31 20:37:18 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndependentStudy::Application.config.secret_key_base = 'aa52b40f374c09657191804dc15c0889b6429eb73c1c573446f865897d8a6aa0d3e44536304fa0e5dfa166b0cfa0268b7edbbb2e3b596a4262dd212c5a18e5e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IndependentStudy::Application.config.session_store :cookie_store, key: '_independent_study_session'
