dc2a46ea184f5b4b861f9d56f7a6333f8c9d72e8
kevinferri <kevinferri@gmail.com>
2014-04-03 00:42:30 +0000

    Added dynamic navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 DefaultApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
